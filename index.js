402b8239d43dcc6199b8aa8b06d71ae448b847cf
Ceren <cerenctn@hotmail.co.uk>
2021-07-13 23:12:06 +0100

    production mode changed back to original

--- index.js
+++ index.js
@@ -12,16 +12,12 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json()); //request body
 
-/*if (process.env.NODE_ENV === "production"){
+if (process.env.NODE_ENV === "production"){
  //server static content
  //npm run build
  app.use(express.static(path.join(__dirname, "client/build")));
-}*/
-if (process.env.NODE_ENV === "production"){
-app.get('*', (req, res) =>{
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
-});
-}
+};
+
 
 //ROUTES
 
